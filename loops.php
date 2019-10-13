8748640ffab8d21770863931d81db9c014b5b743
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2019-10-13 00:35:25 -0700

    working on Loops in php

---
+++ loops.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loops</title>
+</head>
+<body>
+    
+<h1>  "Table of 100"</h1>
+  
+    <?php
+    $j=100;
+    
+    for($i=01;$i<101;$i++){
+        
+        echo "<br>".$j." * ".$i."=".$j*$i;
+    }
+    ?>
+</body>
+</html>
