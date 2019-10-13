b683ddbad571de95b0567cb9bb0f94d5b51567dc
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2019-10-13 09:06:57 -0700

    understanding concept of Comparision Operator in PHP

---
+++ comparisonoperator.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$age = 14;
+if($age < 15){
+    echo "Tayyab azam is not allowed!!!";
+}
+else{
+    echo "Tayyab azam is allowed!!";
+}
+?>
+    
+</body>
+</html>
